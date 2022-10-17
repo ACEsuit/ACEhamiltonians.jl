2ac7c9212d409a6a9e63c2f5f8ca89aed6233045
zhanglw0521 <zhanglw@lsec.cc.ac.cn>
2022-10-16 23:14:10 -0700

    test modified

--- test/matrix_tolerance_tests.jl
+++ test/matrix_tolerance_tests.jl
@@ -86,9 +86,9 @@ function predict_values(H_osb, Rs, on_site=True)
 
     # Create the initial basis entity.
     if on_site
-        basis = OnsiteBasis(20.0, 8, 2, ℓ₁, ℓ₂)
+        basis = OnsiteBasis(20.0, 5, 2, ℓ₁, ℓ₂; species = [:Al, ])
     else
-        basis = OffsiteBasis(20.0, 8, 2, ℓ₁, ℓ₂)
+        basis = OffsiteBasis(20.0, 5, 2, ℓ₁, ℓ₂; species = [:Al, ])
     end
 
     # Calculate the Tychonov regularisation term as per equation
@@ -151,6 +151,8 @@ end
                 # Collect the on-site sub-blocks of the Hamiltonian matrix & the
                 # positions of the neighbouring atoms.
                 H_osb, Rs = block[1], block[3]
+                
+                @show name
 
                 # Hack to deal with type inconsistency issues
                 if name == "dd" 
@@ -215,6 +217,7 @@ end
             for (block, name) in zip(data, off_site_block_names)
                 H_block, Rs = block[1], block[3]
                 
+                @show name
                 if name == "dd" # Hack to deal with type inconsistency issues
                     H_block = reshape.(H_block, 1, 1)
                 end
